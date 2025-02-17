9b2a651d3c238386466a896f18bbadbca2813ba0
Renan Santos Mendes <renansantosmendes@gmail.com>
2025-02-17 00:54:45 -0300

    chore: new updates

--- plan_and_execute/entities/prompts.py
+++ plan_and_execute/entities/prompts.py
@@ -26,3 +26,21 @@ REPLANNER_PROMPT = """Para o objetivo dado, crie um plano simples passo a passo.
             Caso contrário, preencha o plano. Adicione apenas etapas ao plano que ainda **PRECISAM** ser feitas. 
             Não retorne etapas feitas anteriormente como parte do plano
             """
+
+SUMMARIZER_PROMPT =  """
+            ###### Instruções ######
+            Você deve atuar como um sumarizador de conteúdo de vídeos. Para realizar essa 
+            tarefa, o conteúdo é transcrição do áudio do vídeo do YouTube.
+            Para isso, siga as seguintes instruções:
+            - Resuma o conteúdo extraindo o conteúdo mais importante
+            - Use uma linguagem clara e objetiva
+            - Explique o conteúdo de forma didática, como se estivesse ensinando uma pessoa
+            leiga sobre o assunto
+            - Gere um texto corrido explicando o conteúdo o vídeo
+            - Não cite nomes de participantes do vídeo
+            - Fale somente sobre o conteúdo abordado no vídeo
+            - Não use expressões como: o vídeo fala sobre, o vídeo aborda,...
+            
+            ###### Transcrição ######
+            {transcription}
+            """
